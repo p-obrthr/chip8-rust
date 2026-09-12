681464c831b469bdb8f54ec84a75dd650f1dab29
p-obrthr <soc_obthr@web.de>
2026-09-12 10:47:55 +0200

    rm sdl for now

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
-extern crate sdl2;
-use std::fmt;
-
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-
 use std::error::Error;
-use std::time::Duration;
+use std::fmt;
 
 const START_IND: usize = 0x200;
 
@@ -24,40 +17,4 @@ fn main() -> Result<(), Box<dyn Error>> {
     chip8.run()?;
 
     Ok(())
-
-    // let sdl_context = sdl2::init().unwrap();
-    // let video_subsystem = sdl_context.video().unwrap();
-
-    // let window = video_subsystem
-    //     .window("rust-sdl2 demo", 800, 600)
-    //     .position_centered()
-    //     .build()
-    //     .unwrap();
-
-    // let mut canvas = window.into_canvas().build().unwrap();
-
-    // canvas.set_draw_color(Color::RGB(0, 255, 255));
-    // canvas.clear();
-    // canvas.present();
-    // let mut event_pump = sdl_context.event_pump().unwrap();
-    // let mut i = 0;
-    // 'running: loop {
-    //     i = (i + 1) % 255;
-    //     canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-    //     canvas.clear();
-    //     for event in event_pump.poll_iter() {
-    //         match event {
-    //             Event::Quit { .. }
-    //             | Event::KeyDown {
-    //                 keycode: Some(Keycode::Escape),
-    //                 ..
-    //             } => break 'running,
-    //             _ => {}
-    //         }
-    //     }
-    //     // The rest of the game loop goes here...
-
-    //     canvas.present();
-    //     ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    // }
 }
